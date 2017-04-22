78c77919b00d383250589dbf321bcc6f2d7bcac5
Mihai Chezan <mihai.chezan@softgress.com>
2017-04-22 23:41:37 +0300

    wip - hide prefs from legacy

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const prefs = require('sdk/simple-prefs');
-const webext = require("sdk/webextension");
-webext.startup().then(({browser}) => {
+require("sdk/webextension").startup().then(({browser}) => {
   browser.runtime.onConnect.addListener(port => {
     if (port.name === "sync-legacy-addon-data") {
-      port.postMessage(prefs.prefs);
+      port.postMessage(require('sdk/simple-prefs').prefs);
     }
   });
 });
