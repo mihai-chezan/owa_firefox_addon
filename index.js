1ca8535c12513c01d2f55d6aa139e256c9620d00
Mihai Chezan <mihai.chezan@softgress.com>
2017-04-24 01:11:53 +0300

    wip - filter legacy prefs and send only what's needed

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require("sdk/webextension").startup().then(({browser}) => {
   browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.cmd === "get-prefs") {
-      sendResponse(require('sdk/simple-prefs').prefs);
+      const allPrefs = require('sdk/simple-prefs').prefs;
+      // prefs contain other non needed prefs, so filter them and send only what's needed
+      sendResponse({
+    	"delayBetweenChecks": allPrefs.delayBetweenChecks,
+    	"delayBetweenReminders": allPrefs.delayBetweenReminders,
+    	"updateFavIcon": allPrefs.updateFavIcon,
+    	"updateDocumentTitle": allPrefs.updateDocumentTitle,
+    	"cssForUnreadEmailsDetection": allPrefs.cssForUnreadEmailsDetection,
+    	"cssForVisibleRemindersDetection": allPrefs.cssForVisibleRemindersDetection
+      });
     }
   });
 });
