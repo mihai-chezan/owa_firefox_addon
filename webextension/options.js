6f6ad6b6dc386a29e97c2a139714c3fefd8c3504
Mihai Chezan <mihai.chezan@softgress.com>
2017-05-07 01:36:20 +0300

    wip - add known css selectors to a list to allow less knowledgeable people try them #15

--- webextension/options.js
+++ webextension/options.js
@@ -17,18 +17,53 @@ function defaultVal(value, defaultValue){
   return (value === undefined) ? defaultValue : value;
 }
 
+function hide(e) {
+  e.style.visibility = "hidden";
+}
+
+function show(e) {
+  e.style.visibility = "visible";
+}
+
+function setCss(txtInputId, selectId, value) {
+  const val = defaultVal(value, "").trim();
+  const select = document.querySelector(selectId);
+  select.value = val;
+  if (select.selectedIndex === -1) {
+    select.selectedIndex = 1;
+  }
+  const txtInput = document.querySelector(txtInputId);
+  txtInput.value = val;
+  if (select.selectedIndex === 0) {
+    hide(txtInput);
+  }
+}
+
 function restoreOptions() {
   browser.storage.local.get().then((prefs) => {
     document.querySelector("#delayBetweenChecks").value = defaultVal(prefs.delayBetweenChecks, 1);
     document.querySelector("#delayBetweenReminders").value = defaultVal(prefs.delayBetweenReminders, 300);
     document.querySelector("#updateFavIcon").checked = defaultVal(prefs.updateFavIcon, true);
     document.querySelector("#updateDocumentTitle").checked = defaultVal(prefs.updateDocumentTitle, true);
-    document.querySelector("#cssForUnreadEmailsDetection").value = defaultVal(prefs.cssForUnreadEmailsDetection, "");
-    document.querySelector("#cssForVisibleRemindersDetection").value = defaultVal(prefs.cssForVisibleRemindersDetection, "");
-    document.querySelector("#cssForChatNotificationsDetection").value = defaultVal(prefs.cssForChatNotificationsDetection, "");
+    setCss("#cssForUnreadEmailsDetection", "#selectCssForUnreadEmailsDetection", prefs.cssForUnreadEmailsDetection);
+    setCss("#cssForVisibleRemindersDetection", "#selectCssForVisibleRemindersDetection", prefs.cssForVisibleRemindersDetection);
+    setCss("#cssForChatNotificationsDetection", "#selectCssForChatNotificationsDetection", prefs.cssForChatNotificationsDetection);
   }, console.error);
+}
+
+function dropdownChange() {
+  const txtInput = this.nextElementSibling;
+  txtInput.value = this.value;
+  (this.selectedIndex === 0) ? hide(txtInput) : show(txtInput);
+  if (this.selectedIndex === 1) {
+    txtInput.focus();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
+document.getElementById("selectCssForUnreadEmailsDetection").addEventListener("change", dropdownChange);
+document.getElementById("selectCssForVisibleRemindersDetection").addEventListener("change", dropdownChange);
+document.getElementById("selectCssForChatNotificationsDetection").addEventListener("change", dropdownChange);
+
 
