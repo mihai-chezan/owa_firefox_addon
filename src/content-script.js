6d0e60b35c8b34d10658f095b5eae0a9cd0f6d94
Mihai Chezan <mihai.chezan@gmail.com>
2018-08-18 01:10:44 +0200

    fix missing configuration defaults on fresh install (see #21)

--- src/content-script.js
+++ src/content-script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-let prefs = { favIconColor: "#0099FF" };
+let prefs = {
+  favIconColor : "#0099FF"
+};
 let newEventsTimer, remindersTimer;
 let unreadEmailsCount = 0;
 let visibleRemindersCount = 0;
@@ -26,7 +28,7 @@ function getCurrentFavIcon() {
   let icons = document.head.querySelectorAll("link[rel*=icon]");
   let icon;
   if (icons.length > 0) {
-    icon = icons[icons.length-1];
+    icon = icons[icons.length - 1];
   } else {
     icon = createIconElement();
   }
@@ -38,20 +40,21 @@ function generateTabIcon(number) {
   canvas.width = 16;
   canvas.height = 16;
   const ctx = canvas.getContext("2d");
-  // draw cliped circle (radius is bigger than canvas by 1px) - the effect is a square with round corners
+  // draw cliped circle (radius is bigger than canvas by 1px) - the effect is a
+  // square with round corners
   ctx.fillStyle = prefs.favIconColor;
   ctx.beginPath();
-  ctx.arc(8, 8, 9, 0, 2*Math.PI);
+  ctx.arc(8, 8, 9, 0, 2 * Math.PI);
   ctx.fill();
   // draw the number in center
   ctx.font = "bold 12px Helvetica, sans-serif";
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
   ctx.fillStyle = "white";
-  ctx.shadowBlur=1;
-  ctx.shadowColor="black";
-  ctx.shadowOffsetX=1;
-  ctx.shadowOffsetY=1;
+  ctx.shadowBlur = 1;
+  ctx.shadowColor = "black";
+  ctx.shadowOffsetX = 1;
+  ctx.shadowOffsetY = 1;
   ctx.fillText(number, 8, 8);
   return canvas.toDataURL("image/png");
 }
@@ -77,21 +80,21 @@ function restoreInitialDocumentTitle() {
 function setDocumentTitle(emails, reminders, chats) {
   let countPrefix = "";
   if (emails > 0 || reminders > 0 || chats > 0) {
-	countPrefix = "(" + emails + "/" + reminders;
-	if (chats > 0) {
-	  countPrefix = countPrefix + "/" + chats;
-	}
-	countPrefix = countPrefix + ") ";
+    countPrefix = "(" + emails + "/" + reminders;
+    if (chats > 0) {
+      countPrefix = countPrefix + "/" + chats;
+    }
+    countPrefix = countPrefix + ") ";
   }
   document.title = countPrefix + initialDocumentTitle;
 }
 
 function extractNumber(text) {
   if (text) {
-	let digits = text.match(/\d/gi);
-	if (digits) {
-	  return parseInt(digits.join(""), 10);
-	}
+    let digits = text.match(/\d/gi);
+    if (digits) {
+      return parseInt(digits.join(""), 10);
+    }
   }
   return 0;
 }
@@ -99,9 +102,9 @@ function extractNumber(text) {
 function getCountFromNodes(nodes) {
   let count = 0;
   if (nodes) {
-	for (let i = nodes.length - 1; i >= 0; i--) {
-	  count += extractNumber(nodes[i].innerHTML);
-	}
+    for (let i = nodes.length - 1; i >= 0; i--) {
+      count += extractNumber(nodes[i].innerHTML);
+    }
   }
   return count;
 }
@@ -113,7 +116,7 @@ function getItemsWithActiveCount(folder) {
 function getCountFromFolders(folders) {
   let count = 0;
   for (let i = folders.length - 1; i >= 0; i--) {
-	count += getCountFromNodes(getItemsWithActiveCount(folders[i]));
+    count += getCountFromNodes(getItemsWithActiveCount(folders[i]));
   }
   return count;
 }
@@ -121,16 +124,16 @@ function getCountFromFolders(folders) {
 function countUnreadEmails() {
   let nodes;
   if (prefs.cssForUnreadEmailsDetection) {
-	// custom css
-	return getCountFromNodes(document.querySelectorAll(prefs.cssForUnreadEmailsDetection));
+    // custom css
+    return getCountFromNodes(document.querySelectorAll(prefs.cssForUnreadEmailsDetection));
   }
   if ((nodes = document.querySelectorAll("#spnUC #spnCV")).length > 0) {
-	// OWA 2010
-	return getCountFromNodes(nodes);
+    // OWA 2010
+    return getCountFromNodes(nodes);
   }
   if ((nodes = document.querySelectorAll("[aria-label='Folder Pane']")).length > 0) {
-	// OWA 2013
-	return getCountFromFolders(nodes);
+    // OWA 2013
+    return getCountFromFolders(nodes);
   }
   return 0;
 }
@@ -138,16 +141,16 @@ function countUnreadEmails() {
 function countVisibleReminders() {
   let nodes;
   if (prefs.cssForVisibleRemindersDetection) {
-	// custom css
-	return getCountFromNodes(document.querySelectorAll(prefs.cssForVisibleRemindersDetection));
+    // custom css
+    return getCountFromNodes(document.querySelectorAll(prefs.cssForVisibleRemindersDetection));
   }
   if ((nodes = document.querySelectorAll("#spnRmT.alertBtnTxt")).length > 0) {
-	// OWA 2010
-	return extractNumber(nodes[0].innerHTML);
+    // OWA 2010
+    return extractNumber(nodes[0].innerHTML);
   }
   if ((nodes = document.querySelectorAll("[aria-label='New Notification']")).length > 2) {
-	// OWA 2013
-	return extractNumber(nodes[3].title);
+    // OWA 2013
+    return extractNumber(nodes[3].title);
   }
   if ((nodes = document.querySelectorAll(".o365cs-notifications-notificationPopup .o365cs-notifications-notificationHeaderText")).length > 0) {
     // 365 new check
@@ -188,7 +191,7 @@ function triggerNotification(type, text) {
   if (!prefs.disableNotifications) {
     browser.runtime.sendMessage({
       "type" : type,
-      "msg": text
+      "msg" : text
     });
   }
 }
@@ -198,9 +201,9 @@ function checkForNewMessages() {
   let newVisibleRemindersCount = countVisibleReminders();
   let newChatNotificationsCount = countChatNotifications();
   let noChange = (newUnreadEmailsCount === unreadEmailsCount) && (newVisibleRemindersCount === visibleRemindersCount)
-	  && (newChatNotificationsCount === chatNotificationsCount);
+      && (newChatNotificationsCount === chatNotificationsCount);
   if (noChange) {
-	return;
+    return;
   }
   if (prefs.updateFavIcon) {
     setFavicon(newUnreadEmailsCount + newVisibleRemindersCount + newChatNotificationsCount);
@@ -209,13 +212,13 @@ function checkForNewMessages() {
     setDocumentTitle(newUnreadEmailsCount, newVisibleRemindersCount, newChatNotificationsCount);
   }
   if (newUnreadEmailsCount > unreadEmailsCount) {
-	triggerNotification("email", buildEmailNotificationMessage(newUnreadEmailsCount - unreadEmailsCount));
+    triggerNotification("email", buildEmailNotificationMessage(newUnreadEmailsCount - unreadEmailsCount));
   }
   if (newVisibleRemindersCount > visibleRemindersCount) {
-	triggerNotification("reminder", buildReminderNotificationMessage(newVisibleRemindersCount - visibleRemindersCount));
+    triggerNotification("reminder", buildReminderNotificationMessage(newVisibleRemindersCount - visibleRemindersCount));
   }
   if (newChatNotificationsCount > chatNotificationsCount) {
-	triggerNotification("chat", "New chat " + singularOrPlural("notification", newChatNotificationsCount) + "!");
+    triggerNotification("chat", "New chat " + singularOrPlural("notification", newChatNotificationsCount) + "!");
   }
 
   unreadEmailsCount = newUnreadEmailsCount;
@@ -225,32 +228,40 @@ function checkForNewMessages() {
 
 function notifyReminders() {
   if (visibleRemindersCount > 0) {
-	triggerNotification("reminder", "You have " + visibleRemindersCount + " " + singularOrPlural("reminder", visibleRemindersCount));
+    triggerNotification("reminder", "You have " + visibleRemindersCount + " " + singularOrPlural("reminder", visibleRemindersCount));
   }
   if (chatNotificationsCount > 0) {
-	triggerNotification("chat", "You have open chat " + singularOrPlural("notification", chatNotificationsCount));
+    triggerNotification("chat", "You have open chat " + singularOrPlural("notification", chatNotificationsCount));
   }
 }
 
 function setNewPrefs(newPrefs) {
   prefs = newPrefs;
-  if (prefs.delayBetweenChecks === undefined || prefs.delayBetweenChecks < 1) {
-	prefs.delayBetweenChecks = 1;
+  // set defaults
+  prefs.delayBetweenChecks = defaultVal(prefs.delayBetweenChecks, 1);
+  prefs.delayBetweenReminders = defaultVal(prefs.delayBetweenReminders, 300);
+  prefs.disableNotifications = defaultVal(prefs.disableNotifications, false);
+  prefs.updateFavIcon = defaultVal(prefs.updateFavIcon, true);
+  prefs.favIconColor = defaultVal(prefs.favIconColor, "#0099FF");
+  prefs.updateDocumentTitle = defaultVal(prefs.updateDocumentTitle, true);
+  if (prefs.delayBetweenChecks < 1) {
+    prefs.delayBetweenChecks = 1;
   }
-  if (prefs.delayBetweenReminders === undefined || (prefs.delayBetweenReminders > 0 && prefs.delayBetweenReminders < 4)) {
-	prefs.delayBetweenReminders = 4;
+  if (prefs.delayBetweenReminders > 0 && prefs.delayBetweenReminders < 4) {
+    prefs.delayBetweenReminders = 4;
   }
-  if (prefs.favIconColor === undefined) {
-    prefs.favIconColor = "#0099FF";
-  }
+}
+
+function defaultVal(value, defaultValue) {
+  return (typeof value === "undefined") ? defaultValue : value;
 }
 
 function stopTimers() {
   if (newEventsTimer) {
-	clearInterval(newEventsTimer);
+    clearInterval(newEventsTimer);
   }
   if (remindersTimer) {
-	clearInterval(remindersTimer);
+    clearInterval(remindersTimer);
   }
 }
 
@@ -259,7 +270,7 @@ function startMonitor() {
   checkForNewMessages();
   newEventsTimer = setInterval(checkForNewMessages, prefs.delayBetweenChecks * 1000);
   if (prefs.delayBetweenReminders) {
-	remindersTimer = setInterval(notifyReminders, prefs.delayBetweenReminders * 1000);
+    remindersTimer = setInterval(notifyReminders, prefs.delayBetweenReminders * 1000);
   }
 }
 
